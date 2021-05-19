41a1fe7ed423bb9fff44f72c53e11c3565aa61f2
Eugene Kvasnyi <e.kvasnyi@gmail.com>
2021-05-19 17:58:58 +0300

    Added: random message generator

---
+++ main.js
@@ -0,0 +1,43 @@
+const messageParts = {
+    'part1':[
+        'I shall look at you out of the corner of my eye, and you will say nothing.' 
+        , 'How far that little candle throws his beams!' 
+        , 'I believe in the immeasurable power of love;'
+        , 'Prayer is not an old woman\'s idle amusemen.'
+        , 'Can a dream be wrong?'
+    ],
+    'part2': [
+        'Words are the source of misunderstandings.'
+        , 'So shines a good deed in a weary world.'
+        , 'that true love can endure any circumstance and reach across any distance.'
+        , 'Properly understood and applied, it is the most potent instrument of action.'
+        , 'Aren\'t dreams God\'s way of telling you things?'
+    ],
+    'author': [
+        'Antoine de Saint-Exupéry'
+        , 'William Shakespeare'
+        , 'Steve Maraboli'
+        , 'Mahatma Gandhi'
+        , 'Ellen Hopkins'
+    ]
+}
+
+
+const messageGenerator = (messageParts) => {
+    
+    const part1Index = Math.floor(Math.random()*messageParts.part1.length);
+    const part2Index = Math.floor(Math.random()*messageParts.part2.length);
+    const author = Math.floor(Math.random()*messageParts.author.length);
+
+
+    const randomMessage = `${messageParts.part1[part1Index]} ${messageParts.part2[part2Index]} ${"\n"} -${messageParts.author[author]}`
+
+    return randomMessage;
+}
+
+
+
+console.log(messageGenerator(messageParts));
+
+
+
